c36344e1e13c3dbed39fb0a3ab8494df29f57241
ivan <igolubev@miem.hse.ru>
2019-10-13 00:28:00 +0300

    background third variant

--- src/views/st-project-view/index.vue
+++ src/views/st-project-view/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="project-view">
-    <div class="project-view__text">view project</div>
+    <el-row class="project-view__title">
+      <el-col :offset="2" :span="11" :xs="11" :sm="11" :lg="11">
+        test test
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -15,15 +19,16 @@ export default {
 <style lang="scss" scoped>
   .project-view {
     background-color: #f5f7fa;// #ecf2fa;
-    min-height: 2696px; // temp
+    min-height: 12696px; // temp
     background-image: url('~@/assets/views/st-project-view/bg.png');
     background-repeat: no-repeat;
     background-position: center top;
-    background-size: 100%;
+    // background-size: 100%;
   }
 
-  @media (min-width: 1440px) { // 1440px - ширина фона + 54px - ширина sidebar
+  @media (min-width: 1494px) { // 1440px - ширина фона + 54px - ширина sidebar
     .project-view {
+      background-size: 100%;
       // background-size: cover;
     }
   }
