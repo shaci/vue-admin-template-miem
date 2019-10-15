57f4deaf91d00a47c84a1599e8ae65329aa9f6b5
ivan <igolubev@miem.hse.ru>
2019-10-16 00:07:27 +0300

    statistic component created

--- src/views/st-project-view/components/StatisticItem.vue
+++ src/views/st-project-view/components/StatisticItem.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="statistic">
-    {{ number }} {{text}} {{imageLink}}
+    <div class="statistic__number">
+      {{ number }}
+    </div>
+    <div class="statistic__text">
+      {{text}}
+    </div>
+    <div class="statistic__image" :style="{'background-image': 'url(' + require(`@/assets/views_images/st-project-view/components/statistic-item/${imageLink}`) + ')'}"></div>
   </div>
 </template>
 
@@ -27,6 +33,49 @@ export default {
 
 <style lang="scss" scoped>
 .statistic {
-
+  position: relative;
+  background: #FFFFFF;
+  border: 1px solid #DFE0EB;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding-left: 13px;
+  padding-top: 14px;
+  padding-bottom: 120px;
+  .statistic__image {
+    position: absolute;
+    width: 80px;
+    height: 80px;
+    // background-color: red;
+    left: 13px;
+    bottom: 13px;
+    background-position: center center;
+    background-repeat: no-repeat;
+  }
+  // &::after {
+  //   content: "";
+  //   position: absolute;
+  //   width: 72px;
+  //   height: 72px;
+  //   // background-color: red;
+  //   left: 13px;
+  //   bottom: 13px;
+  //   background-image: url('~@/assets/views_images/st-project-view/components/workflow/trello.png');
+  // }
+}
+.statistic__number {
+  margin-bottom: 4px;
+  font-size: 26px;
+  line-height: 31px;
+  /* identical to box height */
+  font-weight: 600;
+  color: #303133;
+}
+.statistic__text {
+  // margin-bottom: 35px;
+  font-size: 15px;
+  line-height: 18px;
+  /* identical to box height */
+  font-weight: 500;
+  color: #606266;
 }
 </style>
